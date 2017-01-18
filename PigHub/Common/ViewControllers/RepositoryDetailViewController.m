f7456e4bdcc8b873051ad8e2b6af0aab91b20e08
Liu@iMac <pizza@pizzaliu.com>
2017-01-18 09:57:39 +0800

    add loading indicator to web load

--- PigHub/Common/ViewControllers/RepositoryDetailViewController.m
+++ PigHub/Common/ViewControllers/RepositoryDetailViewController.m
@@ -8,22 +8,45 @@
 
 #import "RepositoryDetailViewController.h"
 
-@interface RepositoryDetailViewController ()
+@interface RepositoryDetailViewController() <UIWebViewDelegate>
 
+@property (nonatomic, strong) UIView *loadingView;
 
 @end
 
-@implementation RepositoryDetailViewController
+@implementation RepositoryDetailViewController 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
     self.title = self.repo.name;
 
-    UIWebView *webView = [[UIWebView alloc] init];
-    [webView setScalesPageToFit:YES];
-    self.view = webView;
+    // loading view
+    int loadingViewWidth = 80;
+    int loadingViewHeight = 80;
+    self.loadingView = [[UIView alloc]initWithFrame:CGRectMake((self.view.frame.size.width - loadingViewWidth)/2.0, (self.view.frame.size.height - loadingViewHeight)/2.0, loadingViewWidth, loadingViewHeight)];
+    self.loadingView.backgroundColor = [UIColor colorWithWhite:0. alpha:0.6];
+    self.loadingView.layer.cornerRadius = 5;
 
+    UIActivityIndicatorView *activityView=[[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+    activityView.center = CGPointMake(self.loadingView.frame.size.width / 2.0, 35);
+    [activityView startAnimating];
+    activityView.tag = 100;
+    [self.loadingView addSubview:activityView];
+    UILabel* lblLoading = [[UILabel alloc]initWithFrame:CGRectMake(0, 48, 80, 30)];
+    lblLoading.text = NSLocalizedString(@"Loading...", @"loading web page");;
+    lblLoading.textColor = [UIColor whiteColor];
+    lblLoading.font = [UIFont fontWithName:lblLoading.font.fontName size:15];
+    lblLoading.textAlignment = NSTextAlignmentCenter;
+    [self.loadingView addSubview:lblLoading];
+
+    // web view
+    UIWebView *webView = [[UIWebView alloc] init];
+    webView.delegate = self;
+    webView.scalesPageToFit = YES;
+
+    self.view = webView;
+    [self.view addSubview:self.loadingView];
 
     NSString *uri = [NSString stringWithFormat:@"https://github.com%@", self.repo.href];
     NSURL *url = [NSURL URLWithString:uri];
@@ -36,5 +59,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - WebViewDelegate
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    self.loadingView.hidden = YES;
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView
+{
+    self.loadingView.hidden = NO;
+}
 
 @end
