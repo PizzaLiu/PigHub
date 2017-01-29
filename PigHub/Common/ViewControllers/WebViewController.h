a66877cf33570c8d75c977a015e0693a008e5109
Liu@iMac <pizza@pizzaliu.com>
2017-01-29 17:26:58 +0800

    remove non use file

--- PigHub/Common/ViewControllers/WebViewController.h
+++ PigHub/Common/ViewControllers/WebViewController.h
@@ -5,10 +5,9 @@
 //  Created by Rainbow on 2017/1/26.
 //  Copyright © 2017年 PizzaLiu. All rights reserved.
 //
+#import <UIKit/UIKit.h>
 
-#import "ViewController.h"
-
-@interface WebViewController : ViewController
+@interface WebViewController : UIViewController
 
 @property (copy, nonatomic) NSString *url;
 
